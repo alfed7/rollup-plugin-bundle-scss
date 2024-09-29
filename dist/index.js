cecc86f2d398f0b2b0d9c1d3c580dac409b5f761
Alexander Fedorenko <alfedmix@gmail.com>
2024-09-29 22:17:41 +0200

    Remove vue compiler.

--- dist/index.js
+++ dist/index.js
@@ -4,35 +4,6 @@ var fs = require('fs');
 var path = require('path');
 var scssBundle = require('scss-bundle');
 
-function _interopDefaultLegacy (e) { return e && typeof e === 'object' && 'default' in e ? e : { 'default': e }; }
-
-function _interopNamespace(e) {
-  if (e && e.__esModule) return e;
-  var n = Object.create(null);
-  if (e) {
-    Object.keys(e).forEach(function (k) {
-      if (k !== 'default') {
-        var d = Object.getOwnPropertyDescriptor(e, k);
-        Object.defineProperty(n, k, d.get ? d : {
-          enumerable: true,
-          get: function () {
-            return e[k];
-          }
-        });
-      }
-    });
-  }
-  n['default'] = e;
-  return Object.freeze(n);
-}
-
-var path__default = /*#__PURE__*/_interopDefaultLegacy(path);
-
-let compiler = null;
-Promise.resolve().then(function () { return /*#__PURE__*/_interopNamespace(require('vue-template-compiler')); })
-  .then((module) => { compiler = module; })
-  .catch(() => {});
-
 function bundleScss({ output, exclusive = true, bundlerOptions = {} } = {}) {
   const files = [];
   const {
@@ -44,31 +15,20 @@ function bundleScss({ output, exclusive = true, bundlerOptions = {} } = {}) {
   return {
     name: 'bundle-scss',
     transform(source, id) {
-      if (/\.scss$/.test(id)) {
+      if (/\.s?css$/.test(id)) {
         files.push({ id, content: source });
         if (exclusive) {
           return { code: `export default ${JSON.stringify(source)}` };
         }
       }
-      if (/\.vue$/.test(id) && compiler) {
-        const { styles } = compiler.parseComponent(source);
-        files.push(
-          ...styles
-            .filter((style) => style.lang === 'scss')
-            .map((style, index) => ({
-              id: `${id}.${index}.scss`,
-              content: style.content,
-            })),
-        );
-      }
       return null;
     },
     async generateBundle(opts) {
-      const outputPath = path__default['default'].resolve(
-        opts.file ? path__default['default'].dirname(opts.file) : opts.dir,
-        output || `${opts.file ? path__default['default'].parse(opts.file).name : 'index'}.scss`,
+      const outputPath = path.resolve(
+        opts.file ? path.dirname(opts.file) : opts.dir,
+        output || `${opts.file ? path.parse(opts.file).name : 'index'}.scss`,
       );
-      await fs.promises.mkdir(path__default['default'].dirname(outputPath), { recursive: true });
+      await fs.promises.mkdir(path.dirname(outputPath), { recursive: true });
       const entryContent = files.map((file) => `@import "${file.id}";`).join('\n');
       await fs.promises.writeFile(outputPath, entryContent);
       const registry = Object.assign({}, ...files.map((file) => ({ [file.id]: file.content })));
--- index.js
+++ index.js
@@ -2,11 +2,6 @@ import { promises as fs } from 'fs';
 import path from 'path';
 import { Bundler } from 'scss-bundle';
 
-let compiler = null;
-import('vue-template-compiler')
-  .then((module) => { compiler = module; })
-  .catch(() => {});
-
 export default function bundleScss({ output, exclusive = true, bundlerOptions = {} } = {}) {
   const files = [];
   const {
@@ -23,17 +18,6 @@ export default function bundleScss({ output, exclusive = true, bundlerOptions = 
         if (exclusive) {
           return { code: `export default ${JSON.stringify(source)}` };
         }
-      }
-      if (/\.vue$/.test(id) && compiler) {
-        const { styles } = compiler.parseComponent(source);
-        files.push(
-          ...styles
-            .filter((style) => style.lang === 'scss')
-            .map((style, index) => ({
-              id: `${id}.${index}.scss`,
-              content: style.content,
-            })),
-        );
       }
       return null;
     },
